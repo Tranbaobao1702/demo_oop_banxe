ba2bb017abf24bf8cbe854954cad63f628e05d53
Akenu <nhatvuok@gmail.com>
2026-04-06 16:46:19 +0700

    đồng bộ với chương trình chính, comment lại lỗi

--- demo_cuahangbanxe/MAIN.java
+++ demo_cuahangbanxe/MAIN.java
@@ -1,9 +1,34 @@
-package test;
+
+import java.lang.reflect.Array;
 import java.util.*;
+import java.time.LocalDate;
+//import models.WarrantyVisit;
 
 enum Status {
     IN_STOCK, SOLD //các giá trị cố định của kiểu status
 }
+
+class WarrantyVisit {
+    private int id;
+    private int warrantyBookId;
+    private Date visitDate;
+    private int kmReading;
+    private ArrayList<String> error;
+    private String technicianNotes;
+
+    public WarrantyVisit(int id, int warrantyBookId, Date visitDate, int kmReading, String error) {
+        this.id = id;
+        this.warrantyBookId = warrantyBookId;
+        this.visitDate = visitDate;
+        this.kmReading = kmReading;
+        this.error.add(error);
+    }
+
+    // Các hàm Getter/Setter cơ bản
+    public void setTechnicianNotes(String notes) { this.technicianNotes = notes; }
+
+}
+
 //====DONG XE====
 class MotorbikeModel{
     private String modelId;// ma xe
@@ -46,14 +71,16 @@ class MotorbikeVersion{
 class MotorbikeInstance {
     private String vin;
     private String engineNumber;
-    //private String CCCD;
     private Status status;
     private MotorbikeVersion version;
-    public MotorbikeInstance(String vin, String engineNumber,MotorbikeVersion version) {
+    private LocalDate importDate;
+
+    public MotorbikeInstance(String vin, String engineNumber,MotorbikeVersion version, LocalDate importDate) {
         this.vin = vin;
         this.engineNumber = engineNumber;
         this.version=version;
         this.status = Status.IN_STOCK;
+        this.importDate = importDate;
     }
     public MotorbikeVersion getversion(){
         return version;
@@ -67,54 +94,70 @@ class MotorbikeInstance {
         return "VIN: " + vin +" | Engine: " + engineNumber +
         " | bikename: " + version.getmodel().getmodelname() +
          " | Color: " + version.getcolor() + " | Status: " + status;
-    }
 }
 
 // ===== Customer =====
 class Customer {
-    private String name;
+    private int id;
+    private String identityCard;
+    private String fullName;
+    private LocalDate dateOfBirth;
     private String phone;
+    private String address;
+    private String email;
+    private ArrayList<Integer> numVehicle = new ArrayList<>();
+    private ArrayList<WarrantyVisit> historyWar = new ArrayList<>();
 
-    public Customer(String name, String phone) {
-        this.name = name;
+    // Constructor khởi tạo khách hàng mới
+    public Customer(String fullName, String phone, int id, String identityCard, LocalDate dateOfBirth, String address, String email) {
+        this.fullName = fullName;
         this.phone = phone;
-    }
-
-    public String toString() {
-        return name + " - " + phone;
+        this.id = id;
+        this.identityCard = identityCard;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.email = email;
     }
 }
 
+
 // ===== SalesOrder =====
-class SalesOrder {
-    private MotorbikeInstance xe;// vin, numberbike, status
-    private Customer customer;// phone, name
+class SaleOrder {
+    private int id;
+    private int customerId;
+    private LocalDate orderDate;
+    private String paymentStatus;
 
-    public SalesOrder(MotorbikeInstance xe, Customer customer) {
-        this.xe = xe;
-        this.customer = customer;
+    public SaleOrder(int id, int customerId, LocalDate orderDate) {
+        this.id = id;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+        this.paymentStatus = "PAID";
     }
 
-    public String toString() {
-        return "Ban xe " + xe.getVin() + " cho " + customer;
-    }
 }
 
 // ===== Warranty =====
 class WarrantyBook {
-    private String vin;// số khung
-    private List<String> history = new ArrayList<>();
+    private String vin;
+    private int customerId;
+    private LocalDate issueDate;
+    private LocalDate expDate;
 
-    public WarrantyBook(String vin) {
+    public WarrantyBook( String vin, int customerId, LocalDate issueDate, LocalDate expDate) {
         this.vin = vin;
+        this.customerId = customerId;
+        this.issueDate = issueDate;
+        this.expDate = expDate;
     }
 
-    public void addRecord(String record) {
-        history.add(record);
+    public void checkWarranty() {
+       
     }
 }
 
-// ===== Service =====
+// ===== Service ===== 
+// hàm này làm cái j đây
 class HeadService {
     private List<MotorbikeInstance> kho = new ArrayList<>();
 
@@ -151,10 +194,11 @@ class HeadService {
 
 // ===== MAIN =====
 public class MAIN {
+    
     public static void main(String[] args) {
 
         // ===== 1. Tạo service =====
-        HeadService service = new HeadService();
+        //HeadService service = new HeadService();
 
         // ===== 2. Tạo Model =====
         MotorbikeModel model1 = new MotorbikeModel("M01", "Vision", "Honda");
@@ -164,32 +208,31 @@ public class MAIN {
         MotorbikeVersion v1 = new MotorbikeVersion("Red", "110cc", "30tr", model1);
         MotorbikeVersion v2 = new MotorbikeVersion("Blue", "125cc", "45tr", model2);
 
-        // ===== 4. Tạo xe thực tế =====
-        MotorbikeInstance xe1 = new MotorbikeInstance("VIN001", "E001", v1);
-        MotorbikeInstance xe2 = new MotorbikeInstance("VIN002", "E002", v2);
+        // // ===== 4. Tạo xe thực tế =====
+        // MotorbikeInstance xe1 = new MotorbikeInstance("VIN001", "E001", v1);
+        // MotorbikeInstance xe2 = new MotorbikeInstance("VIN002", "E002", v2);
 
-        // ===== 5. Nhập kho =====
-        service.addMotorbike(xe1);
-        service.addMotorbike(xe2);
+        // // ===== 5. Nhập kho =====
+        // service.addMotorbike(xe1);
+        // service.addMotorbike(xe2);
 
-        System.out.println("\n--- DANH SACH XE TRONG KHO ---");
-        service.showKho();
+        // System.out.println("\n--- DANH SACH XE TRONG KHO ---");
+        // service.showKho();
 
-        // ===== 6. Bán xe =====
-        Customer c1 = new Customer("Bao", "0123");
-        service.sellMotorbike("VIN001", c1);
+        // // ===== 6. Bán xe =====
+        // Customer c1 = new Customer("Bao", "0123");
+        // service.sellMotorbike("VIN001", c1);
 
-        System.out.println("\n--- SAU KHI BAN ---");
-        service.showKho();
+        // System.out.println("\n--- SAU KHI BAN ---");
+        // service.showKho();
 
-        // ===== 7. Bảo hành =====
-        service.addMaintenance("VIN001", "Thay dau");
-        service.addMaintenance("VIN001", "Bao tri dinh ky");
+        // // ===== 7. Bảo hành =====
+        // service.addMaintenance("VIN001", "Thay dau");
+        // service.addMaintenance("VIN001", "Bao tri dinh ky");
 
-        // ===== 8. Test thêm =====
-        System.out.println("\n--- TEST TRUNG VIN ---");
-        MotorbikeInstance xe3 = new MotorbikeInstance("VIN001", "E003", v1);
-        service.addMotorbike(xe3); // sẽ báo trùng
-
+        // // ===== 8. Test thêm =====
+        // System.out.println("\n--- TEST TRUNG VIN ---");
+        // MotorbikeInstance xe3 = new MotorbikeInstance("VIN001", "E003", v1);
+        // service.addMotorbike(xe3); // sẽ báo trùng
     }
-}
+}}
